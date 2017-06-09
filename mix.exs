8cc4e96ad6379dc036b061d54563e82bf56f8b28
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-06-09 08:56:27 -0700

    Convert to ESpec for testing

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule PhoenixOcticons.Mixfile do
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [espec: :test]
     ]
   end
 
@@ -21,7 +22,8 @@ defmodule PhoenixOcticons.Mixfile do
       {:octicons, "~> 0.3.0"},
       {:phoenix_html, "~> 2.6"},
       {:cmark, "~> 0.7.0", only: :dev},
-      {:ex_doc, "~> 0.16.1", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.16.1", only: :dev, runtime: false},
+      {:espec, "~> 1.4.0", only: :test}
     ]
   end
 end
--- test/phoenix_octicons_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PhoenixOcticonsTest do
-  use ExUnit.Case
-  doctest PhoenixOcticons
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
